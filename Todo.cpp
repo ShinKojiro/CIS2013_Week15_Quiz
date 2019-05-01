b55b8687afaddeca6174619a5b75406bdf36f150
ShinKojiro <42621442+ShinKojiro@users.noreply.github.com>
2019-05-01 17:58:05 -0500

    Creat start

---
+++ Todo.cpp
@@ -0,0 +1,34 @@
+#ifndef TODO_CPP
+#define TODO_CPP
+
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+class Todo{
+
+    private:
+
+    const int LIST_LENGTH;
+    string tasks[LIST_LENGTH];
+
+    public:
+
+    Todo(int l){
+        LIST_LENGTH = l;
+    }
+
+    void addTask(){
+
+    }
+
+    void completeList(){
+
+    }
+
+    void printList(){
+        
+    }
+
+};
---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <iostream>
+#include "Todo.cpp"
+
+using namespace std;
+
+int main(){
+
+    cout << "C++ Todo List, Version 0.0.1 Alpha" << endl;
+
+    
+    cout << "Add to list (a)\nDone list item (d)\nPrint list (p)\nExit list app (x)\n"; 
+    cout << "What you want to do:" << endl;
+
+}
